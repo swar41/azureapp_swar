11f81372de5e0b77fb9e8dbb630b980262672d0b
Swaroop <141357728+swar41@users.noreply.github.com>
2025-12-11 15:33:53 +0530

    Configuartion_settings

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace azureapp_swar.Pages;
 
 public class IndexModel : PageModel
 {
+    public readonly ILogger<IndexModel> ILogger;
+    private readonly IConfiguration _configuration;
+    public IndexModel( ILogger<IndexModel> logger , IConfiguration configuration)
+    {
+        ILogger = logger;
+        this._configuration = configuration;
+        // GreetingMessage = _configuration["Greeting"];
+    }
     public void OnGet()
     {
-
+        ViewData["GreetingMessage"] = _configuration["Greeting"];
     }
 }
